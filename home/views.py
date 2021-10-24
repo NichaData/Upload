b6947c5990450a364ab92845ca7451e7eb7444e1
nichakorn chonchaiyaphum <s28405@cpss.ac.th>
2021-10-24 15:14:24 +0700

    first commit

---
+++ home/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+
+from django.core.files.storage import FileSystemStorage
+
+
+
+def index(request):
+    context={'a':1}
+    return render(request,'index.html',context)
+
+def predictImage(request):
+    print(request)
+    print(request.POST.dict())
+    fileObj = request.FILES['filePath']
+    fs=FileSystemStorage()
+    filePathName = fs.save(fileObj.name,fileObj)
+    filePathName = fs.url(filePathName)
+    context={'filePathName':filePathName}
+    return render(request,'index.html',context)
